2bef998563a8980a96936c777e10f463d572ad28
mark0613 <goldffd700@gmail.com>
2023-01-21 18:43:47 +0800

    feat: create article model

--- article/models.py
+++ article/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField('title', max_length=255)
+    content = models.TextField('content')
+    time = models.DateTimeField('time', default=datetime.now)
+
+    def __str__(self):
+        return str(self.id) + ": " + self.title
