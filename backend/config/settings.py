948d796c08bb7a50e21ac8f49cd95384cfb1b7bd
mark0613 <goldffd700@gmail.com>
2023-01-23 13:27:13 +0800

    refeator: 重構專案

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 PROJECT_DIR = Path(__file__).resolve().parent.parent.parent
 
-load_dotenv(PROJECT_DIR / '.env')
+load_dotenv(PROJECT_DIR / 'backend/.env')
 
 sys.path.append(os.getenv('ENV_PATH'))
 
