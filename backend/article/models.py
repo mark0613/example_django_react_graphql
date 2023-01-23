51074390b4a562c84af4594be220e81e39136287
mark0613 <goldffd700@gmail.com>
2023-01-23 19:35:43 +0800

    feat: set title is unique

--- backend/article/models.py
+++ backend/article/models.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 
 class Article(models.Model):
-    title = models.CharField('title', max_length=255)
+    title = models.CharField('title', max_length=255, unique=True)
     content = models.TextField('content')
     time = models.DateTimeField('time', default=datetime.now)
 
