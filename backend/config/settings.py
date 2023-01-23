38f9f4c1b76de5b9c68b3ee71711cd1919ab0efa
mark0613 <goldffd700@gmail.com>
2023-01-23 18:39:52 +0800

    refactor: reset .env path with BASE_DIR

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 PROJECT_DIR = Path(__file__).resolve().parent.parent.parent
 
-load_dotenv(PROJECT_DIR / 'backend/.env')
+load_dotenv(BASE_DIR / '.env')
 
 sys.path.append(os.getenv('VENV_PATH'))
 
