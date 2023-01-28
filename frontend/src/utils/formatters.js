2dc94a2f9babc807ccd297a34e14f71a9712d1c5
mark0613 <goldffd700@gmail.com>
2023-01-28 19:26:45 +0800

    fix: 錯誤變數名稱

--- frontend/src/utils/formatters.js
+++ frontend/src/utils/formatters.js
@@ -2,7 +2,7 @@ import Moment from 'moment';
 
 
 export class DateFormatter {
-    static datetime(datetimeString, formt="YYYY-MM-DD HH:mm:ss") {
-        return Moment(datetimeString).format('YYYY-MM-DD HH:mm:ss')
-    };
-}
+    static datetime(datetimeString, format="YYYY-MM-DD HH:mm:ss") {
+        return Moment(datetimeString).format(format);
+    }
+};
