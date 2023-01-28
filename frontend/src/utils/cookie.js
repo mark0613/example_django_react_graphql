eddf5d9f863d3cc60995fb2ae1b4b03e4d85cc5f
mark0613 <goldffd700@gmail.com>
2023-01-28 23:53:58 +0800

    feat: cookie util

---
+++ frontend/src/utils/cookie.js
@@ -0,0 +1,30 @@
+import Cookies from 'universal-cookie';
+
+
+const cookies = new Cookies();
+
+export class Cookie {
+    static set(key, value) {
+        cookies.set(
+            key,
+            value, 
+            { 
+                path: '/',
+                secure: true,
+                sameSite :true
+            }
+        );
+    }
+
+    static get(key) {
+        return cookies.get(key);
+    }
+
+    static contains(key) {
+        return cookies.getAll().hasOwnProperty(key);
+    }
+
+    static remove(key) {
+        cookies.remove(key, { path: '/' });
+    }
+}
